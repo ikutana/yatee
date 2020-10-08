f1c6f8817105325afcf11f257ae6eabab7517ee9
ikutana <ikutana@gmail.com>
2020-10-08 20:53:19 +0900

    コマンドラインの解釈を大胆に変更。後、usage()関数も追加。

--- yatee.cpp
+++ yatee.cpp
@@ -1,29 +1,56 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define	FILE_OPEN_ERROR	1
+
+void usage()
+{
+	printf("usage: yetee [-a|-h] FileName¥n");
+	exit(0);
+}
 
 int main(int argc, char* argv[])
 {
 	int	tmp;
 	FILE	*fp;
-	if (argc==2) {
-		if((fp=fopen(argv[1],"w"))==NULL)
+
+	int argp=1;
+	int appendMode = false;
+	int teeMode = false;
+	char *FileName;
+	while (argp < argc)
+	{
+		if (argv[argp][0]=='-')
 		{
-			fprintf(stderr,"File Open Error");
-			return 1;
+			if (argv[argp][1]=='a')
+				appendMode = true;
+			if (argv[argp][1]=='h')
+				usage();
+		}
+		else
+		{
+			FileName = argv[argp];
+			teeMode = true;
+		}
+		if (FileName != NULL)
+			break;
+		argp++;
+	}
+
+	if (FileName != NULL)
+	{
+		fp = fopen(FileName, appendMode?"a":"w");
+		if (fp == NULL)
+		{
+			fprintf(stderr, "File Open Error¥n");
+			return FILE_OPEN_ERROR;
 		}
 	}
 
-	if (argc>2)
-	{
-		if	(argv[1][1]=='a')
-			fp=fopen(argv[2],"a+");
-		else
-			fp=fopen(argv[2],"c");
-	}
 	while(!feof(stdin))
 	{
 		tmp=fgetc(stdin);
 		fputc(tmp,stdout);
-		if(argc!=1) fputc(tmp,fp);
+		if(fp != NULL) fputc(tmp,fp);
 	}
 	return 0;
 }
